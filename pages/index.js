88294ba50faed064205dd118c97ed40b29177d25
observatoriosanlorenzo-maker <observatoriosanlorenzo@gmail.com>
2025-11-17 12:41:55 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Estación Meteorológica Salta</h1>
+      <p>Página base, después la lleno con tus datos automáticos.</p>
+    </div>
+  );
+}
